dbb25aa82ea81db296d8eb0e8b68341e0c0a7c96
Gabriel <leirbagaboo@gmail.com>
2022-11-29 22:37:21 -0400

    functionality done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,161 @@
 import "./App.css";
 import "./components/Metronome.css";
 import Metronome from "./components/Metronome";
+import { useEffect, useState } from "react";
+
 function App() {
+  // desde aqui me traje esto del componente Metronome xd pa globalizarlo
+  const [bpm, setBpm] = useState(140);
+  const [beatsPerMeasure, setBeatsPerMeasure] = useState(4);
+  const [tempoExpression, setTempoExpression] = useState("allegro");
+
+  useEffect(() => {
+    if (bpm < 20) {
+      setTempoExpression("Larghissimo");
+    } else if (bpm == 20) {
+      setTempoExpression("Largo");
+    } else if (bpm < 40) {
+      setTempoExpression("Lento moderato");
+    } else if (bpm == 40) {
+      setTempoExpression("Grave");
+    } else if (bpm < 60) {
+      setTempoExpression("Lento");
+    } else if (bpm < 66) {
+      setTempoExpression("Larghetto");
+    } else if (bpm < 76) {
+      setTempoExpression("Adagio");
+    } else if (bpm < 80) {
+      setTempoExpression("Adagietto");
+    } else if (bpm < 108) {
+      setTempoExpression("Andante");
+    } else if (bpm < 168) {
+      setTempoExpression("Allegro");
+    } else if (bpm < 200) {
+      setTempoExpression("Presto");
+    } else if (bpm < 240) {
+      setTempoExpression("Prestissimo");
+    } else if (bpm > 240) {
+      setTempoExpression("Allegro prestissimo con fuoco");
+    }
+  }, [bpm]);
+
+  function decreaseTempo() {
+    // setPlaying(!playing);
+    resetTimer();
+    setBpm(bpm > 10 ? bpm - 1 : (bpm = 10));
+  }
+
+  function increaseTempo() {
+    // setPlaying(!playing);
+    resetTimer();
+    setBpm(bpm < 280 ? bpm + 1 : (bpm = 280));
+  }
+
+  const handleBpmChange = (e) => {
+    if (playing) {
+      setPlaying(!playing);
+      resetTimer();
+    }
+
+    setBpm(+e.target.value);
+    // setPlaying(e.target.value);
+  };
+
+  function subtracBeat() {
+    // setPlaying(!playing);
+    resetTimer();
+    setBeatsPerMeasure(
+      beatsPerMeasure > 1 ? beatsPerMeasure - 1 : beatsPerMeasure
+    );
+  }
+
+  function addBeat() {
+    // setPlaying(!playing);
+    resetTimer();
+
+    setBeatsPerMeasure(
+      beatsPerMeasure < 16 ? beatsPerMeasure + 1 : beatsPerMeasure
+    );
+  }
+  // hasta aqui y luego le pase cada cosa como prop al propio componente cuando lo llamo en app
+  //le paso esas props y las recibe como arg en el componente funcional
+
+  // // es porque quiero agarrar el bpm por ejemplo como variable global
+  // y a futuro sincronizarla con la aparicion de proposiciones ritmicas
+
+  const [playing, setPlaying] = useState(false);
+  const [timer, setTimer] = useState(0);
+
+  const click1 = new Audio("/click1.mp3");
+  const click2 = new Audio("/click2.mp3");
+
+  let count = 0;
+
+  // function play() {
+  //    click1.play();
+
+  // }
+
+  function handleStartStop() {
+    setPlaying(!playing);
+    if (!playing) {
+      setTimer(setInterval(play, (60 / bpm) * 1000));
+      count = 0;
+    } else {
+      resetTimer();
+    }
+  }
+
+  function play() {
+    if (count % beatsPerMeasure === 0) {
+      click1.play();
+    } else {
+      click2.play();
+    }
+    count = count + (1 % beatsPerMeasure);
+  }
+
+  function resetTimer() {
+    clearInterval(timer);
+    setTimer(0);
+  }
+
+  //  const handlePlay = () => {
+  //   if(playing){
+  //     setPlaying (false)
+  //     clearInterval(play)
+  //   }else{
+  //     setPlaying (true);
+  //     setInterval(play, (60/bpm) * 1000)
+  //   }
+
+  // }
+
+  let text;
+
+  if (playing) {
+    text = "stop";
+  } else {
+    text = "play";
+  }
+
+  console.log(playing);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Metronome />
+        <Metronome
+          bpm={bpm}
+          tempoExpression={tempoExpression}
+          beatsPerMeasure={beatsPerMeasure}
+          decreaseTempo={decreaseTempo}
+          increaseTempo={increaseTempo}
+          handleBpmChange={handleBpmChange}
+          subtracBeat={subtracBeat}
+          addBeat={addBeat}
+          handleStartStop={handleStartStop}
+          text={text}
+        />
       </header>
     </div>
   );
--- src/components/Metronome.js
+++ src/components/Metronome.js
@@ -1,24 +1,101 @@
-function Metronome() {
-  let tempoExpression = "Allegro prestissimo con fuoco";
+function Metronome({
+  bpm,
+  tempoExpression,
+  beatsPerMeasure,
+  decreaseTempo,
+  increaseTempo,
+  handleBpmChange,
+  subtracBeat,
+  addBeat,
+  playing,
+  handleStartStop,
+  text,
+}) {
+  //buenas buenas, me saque esta lista de expresiones de wikipedia
+  // algunas no tenian valor metronomico(asi que esas no las inclui)
+  //el salto de linea es para medio ubicar donde estan esas sin valor
+  //hay algunas que ya no se usan o que su valor es especificamente 1 numero e igual las inclui
+  //fin del comunicado (igual esto es como pa leerlo yo mismo :'c)
+
+  // Larghissimo: extremadamente lento (menos de 20 ppm); usado en raras ocasiones.
+  // Largo: muy lento (20 ppm).
+  // Lento moderato: (20 - 40ppm).
+  // Lento: lento (40 - 60 ppm).
+  // Grave: lento y solemne (≈40 ppm).
+  // Larghetto: más o menos lento (60 - 66 ppm)
+  // Adagio: lento y majestuoso (66 - 76 ppm); para Clementi, el movimiento más largo no era el largo sino el adagio.10​
+  // Adagietto: un poco menos lento que el adagio (70 - 80 ppm); poco usado.
+
+  // Tranquillo: tranquilo.
+  // Tranquillamente.
+
+  // Afettuoso: (72 ppm).
+
+  // Andante: al paso, tranquilo, un poco vivaz (76 - 108 ppm).
+  // Andante moderato: con un poco más de celeridad que el andante (92 - 112 ppm).
+  // Andantino: más vivo que el andante moderato; sin embargo, para algunos significa menos vivo que el andante.
+  // Moderato espressivo
+  // Moderato: moderado (80 - 108 ppm).
+
+  // Allegretto grazioso.
+  // Allegretto: un poco animado; sin embargo, en algunas piezas se toca como allegro y en otras como andante.
+  // Allegro moderato.
+
+  // Allegro: animado y rápido (110 - 168 ppm).
+  // Vivace: vivaz.
+  // Vivo: rápido y vivaz.
+  // Allegrissimo: más rápido que el allegro; poco usado.
+
+  // Presto: muy rápido (168 - 200 ppm).
+  // Vivacissimo: más rápido que el vivace; poco usado.
+  // Vivacissimamente: Más rápido que el Vivacissimo.
+
+  // Prestissimo: muy rápido (más de 200 ppm).
+  // Allegro prestissimo con fuoco: extremadamente rápido (más de 240 ppm).
 
   return (
     <div className="container">
       <div className="metronome">
+        {/* <p>buenas buenas a mi boton no le da la gana de hacer toggle salu2 </p> */}
         <div className="bpm-display">
-          <span className="tempo">140</span>
+          <span className="tempo">{bpm}</span>
           <span className="bpm">BPM</span>
         </div>
         <div className="tempo-expression">{tempoExpression}</div>
         <div className="tempo-settings">
-          <div className="adjust-tempo-btn decrease-tempo">-</div>
-          <input type="range" min={10} max={280} step={1} />
-          <div className="adjust-tempo-btn increase-tempo">+</div>
+          <div
+            className="adjust-tempo-btn decrease-tempo"
+            onClick={decreaseTempo}
+          >
+            -
+          </div>
+          <input
+            type="range"
+            min={10}
+            max={280}
+            step={1}
+            className="slider"
+            value={bpm}
+            onChange={handleBpmChange}
+          />
+          <div
+            className="adjust-tempo-btn increase-tempo"
+            onClick={increaseTempo}
+          >
+            +
+          </div>
         </div>
-        <div className="start-stop">START</div>
+        <div className="start-stop" onClick={handleStartStop}>
+          {text}
+        </div>
         <div className="measures">
-          <div className="subtract-beats stepper">-</div>
-          <div className="measure-count">4</div>
-          <div className="add-beats stepper">+</div>
+          <div className="subtract-beats stepper" onClick={subtracBeat}>
+            -
+          </div>
+          <div className="measure-count">{beatsPerMeasure}</div>
+          <div className="add-beats stepper" onClick={addBeat}>
+            +
+          </div>
         </div>
         <span className="beats-per-measuse-text">Beats per measure</span>
       </div>
